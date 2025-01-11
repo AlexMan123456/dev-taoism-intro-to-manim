b0524729787e15c65e454d4db579a31d47648c91
AlexMan123456 <alex.man03888@gmail.com>
2025-01-11 15:50:44 +0000

    Completed task 2 of mobject-attrs - made objects appear in a row, aligned by top

---
+++ mobject-attrs/2-aligning-objects.py
@@ -0,0 +1,10 @@
+from manim import *
+
+class AligningObjects(Scene):
+    def construct(self):
+        objects = VGroup(Circle().to_edge(UL), Square(), Star(), Triangle(), Circle())
+
+        for i in range(1,5):
+            objects[i].next_to(objects[i-1], RIGHT, aligned_edge=UP)
+
+        self.add(objects)
