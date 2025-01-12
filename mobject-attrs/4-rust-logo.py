adb3a0bf2f681e4af3222d56d249d5926b4f8b9d
AlexMan123456 <alex.man03888@gmail.com>
2025-01-12 00:37:42 +0000

    Created rust logo

---
+++ mobject-attrs/4-rust-logo.py
@@ -0,0 +1,12 @@
+from manim import *
+
+class RustLogo(Scene):
+    def construct(self):
+        background_circle = Circle(3, WHITE).set_fill(WHITE).set_opacity(1)
+        big_triangle = Triangle(color="#2FA673").set_fill("#2FA673").set_opacity(1).scale(2)
+        medium_triangle = big_triangle.copy().set_color("#2A3C4E").set_fill("#2A3C4E").scale(0.6).next_to(big_triangle, DOWN, aligned_edge=DOWN).shift(1.16*UP)
+        small_triangle = medium_triangle.copy().set_color(WHITE).set_fill(WHITE).scale(1.1).next_to(medium_triangle, DOWN, aligned_edge=DOWN)
+
+        triangles = VGroup(big_triangle, medium_triangle, small_triangle).rotate(PI).move_to(background_circle.get_center()).shift(0.4*UP)
+
+        self.add(background_circle, triangles)
